3ff815a759085d87151f66ec1fa0c58c7bed1223
Robesz <kvrobert@gmail.com>
2018-08-19 19:33:33 +0200

    Rest app

---
+++ src/main/java/com/robesz/testrest/TestrestApplication.java
@@ -0,0 +1,12 @@
+package com.robesz.testrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestrestApplication.class, args);
+    }
+}
